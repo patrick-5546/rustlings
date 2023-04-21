ee1bf3f4214850c1d6dd4786a0bf546c00350344
Patrick Creighton <pcreighton429@gmail.com>
2023-04-20 22:18:28 -0700

    Completed exercises April 20, 2023

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,11 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(quantity: u8) -> u8 {
+    let unit_price = if quantity <= 40 { 2 } else { 1 };
+    quantity * unit_price
+}
 
 // Don't modify this function!
 #[test]
